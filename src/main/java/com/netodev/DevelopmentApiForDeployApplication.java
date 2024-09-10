3fc1c58d0f8a47930547b198baf8f3686934849d
User <user@Neto-MacBook-Pro.local>
2024-09-10 08:09:21 -0300

    Initial commit

---
+++ src/main/java/com/netodev/DevelopmentApiForDeployApplication.java
@@ -0,0 +1,13 @@
+package com.netodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentApiForDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentApiForDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netodev/DevelopmentApiForDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentApiForDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
