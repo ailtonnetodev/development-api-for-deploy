f602b671597a7a796673a003cfd965f1306bd880
ailtonnetodev <ailtonnetto8319@gmail.com>
2024-09-11 08:43:14 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/netodev/domain/model/User.java
@@ -0,0 +1,30 @@
+package com.netodev.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200)
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+}
---
+++ src/main/java/com/netodev/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.netodev.domain.repository;
+
+import com.netodev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
